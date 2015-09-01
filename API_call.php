5f27067d0ba59441db5f6f796867acb9e564b859
EDLyonhart <EDLyonhart@gmail.com>
2015-09-01 11:26:14 -0700

    First API call draft

---
+++ API_call.php
@@ -0,0 +1,42 @@
+<?php
+
+class CampaignInfo
+{
+
+  //  Mask values
+  private $accessToken;
+  private $cid;
+
+  public function __construct($accessToken, $cid) {
+    $this->accessToken = $accessToken;
+    $this->cid = $cid;
+  }
+
+  public function getEvent($eid) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "www.classy.org/api1/campaigns?token=" . $this->accessToken . "&cid=" . $this->cid . "&eid=" . $eid);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec ($ch);
+    echo curl_error($ch);
+    curl_close ($ch);
+
+    $jsonResult = json_decode($result, true);
+    // var_dump($jsonResult);
+
+    $totalRaised = $jsonResult['campaigns'][0]['totalRaised'];
+    print "Total = $totalRaised . ";
+    
+    $goal = $jsonResult['campaigns'][0]['goal'];
+    print "Goal = $goal .";
+  }
+}
+
+$accessToken = '---xxx---xxx';
+$cid = 'xxx---';
+$eid = '---xxx';
+
+$campaign = new CampaignInfo($accessToken, $cid);
+$campaign->getEvent($eid);
+
+?>
