b23df6f959575866e3b86c19ba0042d2ab4fad5f
EDLyonhart <EDLyonhart@gmail.com>
2015-09-07 17:19:34 -0700

    worked on widget. commented out goals

--- classy-api-call/API_call.php
+++ classy-api-call/API_call.php
@@ -12,6 +12,8 @@ class CampaignInfo
     $this->cid = $cid;
   }
 
+
+
   public function getEvent($eid) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "www.classy.org/api1/campaigns?token=" . $this->accessToken . "&cid=" . $this->cid . "&eid=" . $eid);
@@ -24,19 +26,25 @@ class CampaignInfo
     $jsonResult = json_decode($result, true);
     // var_dump($jsonResult);
 
-    $totalRaised = $jsonResult['campaigns'][0]['totalRaised'];
-    print "Total = $totalRaised . ";
-    
+    $totalRaised = $jsonResult['campaigns'][0]['total_raised'];
     $goal = $jsonResult['campaigns'][0]['goal'];
-    print "Goal = $goal .";
   }
 }
 
-$accessToken = 'xxx---xxx---';
-$cid = '---xxx';
-$eid = 'xxx---';
+$accessToken = '---xxx---xxx';
+$cid = 'xxx---';
+$eid = $_POST["eid"];
 
 $campaign = new CampaignInfo($accessToken, $cid);
 $campaign->getEvent($eid);
 
 ?>
+
+
+
+
+
+
+
+
+
--- classy-api-call/classy-api-call.php
+++ classy-api-call/classy-api-call.php
@@ -9,72 +9,109 @@
  * License: None
  */
 
+
+
+/*
+ * Back end display
+ *  - Form which Accepts inputs (EID's and the like)
+ *   - Create Array for storage, store only if 'status_code' return 'SUCCESS'
+ *  - Display previously, successful, searches
+ */
+
 add_action('admin_menu', 'classy_api_call_admin_actions');
 function classy_api_call_admin_actions() {
   add_options_page('Classy API Call', 'Classy API Call', 'manage_options', __FILE__, 'classy_api_call_admin');
 }
 
 function classy_api_call_admin() {
-?>
+  ?>
   <div class="wrap">
     <h2>API Calls</h2>
     <h3>Listed below are the current calls to the Classy API</h3>
     <p>Fill in the form and click the button below to add an API query</p>
     <br>
+
     <form action="" method="POST">
-      <input type="text" name="eventID" value="" placeholder="Input EID here">
+      <input type="text" name="eid" value="" placeholder="Input EID here">
       <input type="submit" name="newEventSearch" value="search" class="button-primary">
     </form>
-    <br>
 
+    <br>
     <table class="widefat">
       <thead>
         <tr>
           <th>Event ID</th>
         </tr>
       </thead>
-
       <tfoot>
         <tr>
           <th>Event ID</th>
         </tr>
       </tfoot>
-
       <tbody>
-<?php
 
-$myEvents = array();
+        <?php
 
-if(isset($_POST('newEventSearch')){
-  // check the API for this event
-  // if status_code === 'SUCCESS'
-    // add it to the $myEvents array as 'eventID'
-    // add status code to the array
-  // else display an error message
-  // update_options('newEventSearch', $myEvents); // store events in the 'options' table
-}
-else if (get_option('newEventSearch')){
-  $myEvents = get_option('myEvents')
-}
+        // if 'myEvents' is empty, initialize it
+        if (empty($myEvents)) {
+          // initialize it
+          $myEvents = array();
+        } else {
+          // otherwise get the previously existing values from options tabel
+        }
+        
 
-  // loop through each element of the $myEvents array
-  // list all events which have successfully been queried in the past
-foreach ($myEvents as $myEvent)
-{
-?>
-        <tr>    
-<?php
-  
-  echo"<td>" . $myEvent->status_code . "</td>";
-}
-?>          
+        // when 'newEventSearch' is submitted:
+        if ($_POST['newEventSearch']) {
+          // add the submitted value into the 'myEvents' array
+          // search to see if the event exists
+            // if the status returns "SUCCESS"
+            // save the updated 'myEvents' into the options tabel
 
+
+
+          array_push($myEvents, $_POST["eid"])
+          $eid_search = $_POST['update'];
+          // if $eid_search->'status-code' === "SUCCESS"
+            //update_options($myEvents)
+          // else return an error
+        }
+
+        get_alloptions('newEventSearch')
+
+
+        // display all values (events) stored in 'myEvents' array
+
+/*
+** Here is the main grind.
+** I want to generate shortcode (or some other way to reference an API call function) for each of these events.
+** This would be to store values needed. 'goal' and 'current-total' are the main ones which come to mind currently.
+**
+*/
+
+        foreach ($myEvents as $myEvent){
+
+          ?>
+          <tr>    
+            <?php
+            // display the value
+            echo"<td>" . $myEvent . "</td>";
+          ?>          
         </tr>
+        <?php } ?>
       </tbody>
     </table>
   </div>
-<?php
+  <?php
 
 }
 
+
+/*
+ * Access to information
+ *  - API call in a place where the returned info can be accessed from the front end?
+ *  - Create Shortcodes or Variables which will be useable by other elements // plugins on the website
+ *   - Used to run more API calls
+ *   - Get fresh information on each page load
+ */
 ?>
