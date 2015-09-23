a7ad03832797313764e0d2b21f8bf013215ab159
EDLyonhart <EDLyonhart@gmail.com>
2015-09-23 11:27:21 -0700

    integrate into existing 'skill' shortcode from AZOOM theme

--- classy-api-call/newCall.php
+++ classy-api-call/newCall.php
@@ -20,12 +20,14 @@ class CampaignInfo
     $result = curl_exec ($ch);
     echo curl_error($ch);
     curl_close ($ch);
+
     
     // make 'jsonResult' available to other functions
     add_filter( 'json_retrieve', 'json_send' )
     function json_send ($arg = '') {
       $jsonResult = json_decode($result, true);
     }
+
 
     // exit if no results returned
     if ($jsonResult['status_code'] != 'SUCCESS' {
@@ -37,31 +39,38 @@ class CampaignInfo
 
 function campaign_one_return( $atts ) {
 
-  // TO DO
-  // rename away from individual campaigns.
+  global $camp_one_name;
+  global $camp_one_total;
+  global $camp_one_progress;
   
-  $campaign1 = new CampaignInfo('xxx---xxx---', 'xxx---');
-  $campaign1->getEvent(---xxx);
+  $campaign1 = new CampaignInfo('xxx---xxx---', '---xxx');
+  $campaign1->getEvent(xxx---);
 
-  $jsonResult = apply_filters( 'json_send', '' );
-
+  global $jsonResult;
   $campaign_one_parse = $jsonResult['campaigns'][0];
   extract(shortcode_atts(array(
     'key' => 'name'
     ), $atts));
 
-  return $campaign_one_parse[$key];
+  $campaign_one_parse = $jsonResult["campaigns"][0];
+  
+  $camp_one_name = $campaign_one_parse["name"];
+  $camp_one_total = $campaign_one_parse["total_raised"];
+  if ( $campaign_one_parse["goal"] < 1 ) {
+    $camp_one_progress = 100;
+  } else {
+    $camp_one_progress = ($campaign_one_parse["total_raised"]/$campaign_one_parse["goal"])*100;
+  }
+  $camp_one = '[rockthemes_skill skill_title="' . $camp_one_name . ' - $ ' . $camp_one_total . '" skill_color="#787878" skill_bg_color="#219cab" skill_current_value="' . $camp_one_progress . '%"]undefined[/rockthemes_skill]';
+  print $camp_one;
 }
-
-add_shortcode('campaign_one', 'campaign_one_return' );
-
 
 /*
 
 Here is what the shotcodes will look like this:
 
 [campaign_one key="name"]
-[campaign_one key="total_raised]
+[campaign_one key="total_raised"]
 [campaign_one key="goal"]
 
 */
